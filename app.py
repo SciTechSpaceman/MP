a880dcfcb9fb8f8b5ca1c43c17ec30fa238a6859
SciTechSpaceman <107294316+SciTechSpaceman@users.noreply.github.com>
2022-07-15 11:43:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+import joblib 
+
+model_nb = joblib.load('Pass-Fail')
+
+st.title("PASS FAIL CLASSIFIER")
+ip = st.text_input("Enter The Marks:")
+
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])  
+  
